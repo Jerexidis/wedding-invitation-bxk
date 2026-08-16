dbcc1d2324344c33a5c1ac7d80d835a196457fa8
Luismtz31 <108112938+Jerexidis@users.noreply.github.com>
2026-08-15 22:30:39 -0600

    deploy: update invitations

--- src/invitations/gretel-y-geraldine/index.jsx
+++ src/invitations/gretel-y-geraldine/index.jsx
@@ -51,7 +51,7 @@ const STORY_CONFIG = {
             place: 'Salón Cumbres',
             address: 'Calle Venustiano Carranza #203, Pabellón de Hidalgo',
             time: 'Después de la ceremonia',
-            maps: 'https://maps.app.goo.gl/V3ycXtG6U1YuPH5MA?g_st=aw',
+            maps: 'https://maps.app.goo.gl/eTUSQ3fGeYkcGRc87',
             icon: PartyPopper,
         },
     ],
